4d9be2fdb9ae7eabfb84d2ab32ab3531ec50a609
Alexandre Rousseau <contact@rousseau-alexandre.Fr>
2018-12-20 09:38:25 +0100

    first try of sonar socket

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, Response
+from flask_socketio import SocketIO, send, emit
+
 from components.LCD.I2CLCD1602 import display
+from components.ultrasonic_ranging import loop_during
 
 
 app = Flask(__name__)
@@ -9,10 +12,11 @@ def hello():
     content = """<h1>Elevator web server</h1>
     <p>An Python Flask web server to master some Raspberry PI components. This server expose some routes:</p>
 
-    <ul> 
-      <li>GET `/lcd/display/<content>`</li>
-      <li>GET `/led/blink`</li>
-    </ul> 
+    <ul>
+      <li>GET <a href="/lcd/display/helloworld">/lcd/display/<content></a></li>
+      <li>GET <a href="/led/blink">/led/blink</a></li>
+      <li>GET <a href="/sonar/run">/sonar/run/<int:time></a></li>
+    </ul>
     """
     return Response(content, mimetype='text/html')
 
@@ -29,3 +33,25 @@ def blink_led():
     """
     # TODO
     pass
+
+
+@app.route('/sonar/run/<int:time>')
+def run_sonar(content):
+    """Simply read content from URI & display message on LCD screen
+    """
+    for i in loop_during(time):
+        display(content)
+        emit('sonar', )
+    return "finished"
+
+
+@socketio.on('message')
+def handle_message(message):
+  print('received message: ' + message)
+
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+  print('received json: ' + str(json))
+  emit('chat', secrets.token_hex(nbytes=8))
--- components/ultrasonic_ranging.py
+++ components/ultrasonic_ranging.py
@@ -24,15 +24,15 @@ def pulseIn(pin,level,timeOut): # function pulseIn: obtain pulse time of a pin
             return 0;
     pulseTime = (time.time() - t0)*1000000
     return pulseTime
-    
+
 def getSonar():     #get the measurement results of ultrasonic module,with unit: cm
-    GPIO.output(trigPin,GPIO.HIGH)      #make trigPin send 10us high level 
+    GPIO.output(trigPin,GPIO.HIGH)      #make trigPin send 10us high level
     time.sleep(0.00001)     #10us
     GPIO.output(trigPin,GPIO.LOW)
     pingTime = pulseIn(echoPin,GPIO.HIGH,timeOut)   #read plus time of echoPin
     distance = pingTime * 340.0 / 2.0 / 10000.0     # the sound speed is 340m/s, and calculate distance
     return distance
-    
+
 def setup():
     print ('Program is starting...')
     GPIO.setmode(GPIO.BOARD)       #numbers GPIOs by physical location
@@ -45,7 +45,7 @@ def loop():
         distance = getSonar()
         print ("The distance is : %.2f cm"%(distance))
         time.sleep(1)
-        
+
 if __name__ == '__main__':     #program start from here
     setup()
     try:
@@ -53,5 +53,17 @@ if __name__ == '__main__':     #program start from here
     except KeyboardInterrupt:  #when 'Ctrl+C' is pressed, the program will exit
         GPIO.cleanup()         #release resource
 
+# CUSTOM AREA
+# -----------
 
-	
+def loop_during(time, interval=1):
+    """ Get sonar value during some time
+
+    - `time` is exprimed in seconds
+    - `interval` is interval between each values exprimed in seconds
+    """
+    GPIO.setup(11,GPIO.IN)
+    while(time > 0):
+        yield getSonar()
+        time = time - interval
+        time.sleep(interval)
